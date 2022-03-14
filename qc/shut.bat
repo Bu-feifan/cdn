a4a82f0350a6a7ad395b9e759315cb413ef504b7
bu-feifan <admin@bufeifan.top>
2022-03-14 11:07:06 +0800

    cdn

--- qc/shut.bat
+++ qc/shut.bat
@@ -1,38 +1,26 @@
 @echo off 
 :start 
-shutdown -a
+color f0
+shutdown -a >nul .
 set a_EL=%ERRORLEVEL%
-if "%1"=="N" goto sd_N
-if "%1"=="F" goto sd_F
-if "%1"=="R" goto s_R
-if "%1"=="RF" goto sd_RF
-if "%1"=="RN" goto sd_RN
-if "%1"=="n" goto sd_N
-if "%1"=="f" goto sd_F
-if "%1"=="r" goto s_R
-if "%1"=="rf" goto sd_RF
-if "%1"=="rn" goto sd_RN
-if "%1"=="rF" goto sd_RF
-if "%1"=="rN" goto sd_RN
-if "%1"=="Rf" goto sd_RF
-if "%1"=="Rn" goto sd_RN
+if /I "%1"=="N" goto sd_N
+if /I "%1"=="F" goto sd_F
+if /I "%1"=="R" goto s_R
+if /I "%1"=="RF" goto sd_RF
+if /I "%1"=="RN" goto sd_RN
 title=电源选项
 
 :which
 cls
-if %a_EL% equ 0 (mode con cols=29 lines=6&color f0
+if %a_EL% equ 0 (mode con cols=29 lines=6
 echo 已取消命令
 echo.
-echo 是否继续？[其他字符=退出]
-echo.) else (mode con cols=37 lines=4&color f0
-echo 是否关机？[Q=退出][其他字符=普通模式])
-set /p s_If=请输入[R=重启][F=快速模式]:
-if "%s_If%"=="Q" goto quit
-if "%s_If%"=="F" goto sd_F
-if "%s_If%"=="R" goto s_R
-if "%s_If%"=="q" goto quit
-if "%s_If%"=="f" goto sd_F
-if "%s_If%"=="r" goto s_R
+echo 是否继续？[其他字符=退出]) else (mode con cols=37 lines=4
+echo 是否关机？[Q=退出][其他字符=5s后关机])
+set /p s_If=请输入[R=重启][F=立即关机]:
+if /I "%s_If%"=="Q" goto quit
+if /I "%s_If%"=="F" goto sd_F
+if /I "%s_If%"=="R" goto s_R
 if %a_EL% equ 0 goto quit
 goto sd_N
 
@@ -45,21 +33,16 @@ shutdown -s -t 0
 goto quit
 
 :s_R
-if "%2"=="F" goto sd_RF
-if "%2"=="N" goto sd_RN
-if "%2"=="f" goto sd_RF
-if "%2"=="n" goto sd_RN
+if /I "%2"=="F" goto sd_RF
+if /I "%2"=="N" goto sd_RN
 cls 
 mode con cols=39 lines=4
-echo 重启选项[F=快速模式][其他字符=普通模式]
+echo 重启选项[F=立即][其他字符=5s后]
 echo. 
 set /p r_F=请输入[Q=退出][B=返回]: 
-if "%r_F%"=="Q" goto quit
-if "%r_F%"=="F" goto sd_RF
-if "%r_F%"=="B" goto s_B
-if "%r_F%"=="q" goto quit
-if "%r_F%"=="f" goto sd_RF
-if "%r_F%"=="b" goto s_B
+if /I "%r_F%"=="Q" goto quit
+if /I "%r_F%"=="F" goto sd_RF
+if /I "%r_F%"=="B" goto s_B
 goto sd_RN
 goto quit
 
@@ -72,9 +55,9 @@ shutdown -r -t 0
 goto quit
 
 :s_B
+set a_EL=u
 set s_If=u
 set r_F=u
-set a_EL=u
 goto which
 
 :quit
--- qc/shut_cmd.bat
+++ qc/shut_cmd.bat
@@ -2,20 +2,11 @@
 :start 
 shutdown -a
 if "%ERRORLEVEL%"=="0" goto quit
-if "%1"=="N" goto sd_N
-if "%1"=="F" goto sd_F
-if "%1"=="R" goto s_R
-if "%1"=="RF" goto sd_RF
-if "%1"=="RN" goto sd_RN
-if "%1"=="n" goto sd_N
-if "%1"=="f" goto sd_F
-if "%1"=="r" goto s_R
-if "%1"=="rf" goto sd_RF
-if "%1"=="rn" goto sd_RN
-if "%1"=="rF" goto sd_RF
-if "%1"=="rN" goto sd_RN
-if "%1"=="Rf" goto sd_RF
-if "%1"=="Rn" goto sd_RN
+if /I "%1"=="N" goto sd_N
+if /I "%1"=="F" goto sd_F
+if /I "%1"=="R" goto s_R
+if /I "%1"=="RF" goto sd_RF
+if /I "%1"=="RN" goto sd_RN
 :sd_N
 shutdown -s -t 5
 goto quit
@@ -23,8 +14,7 @@ goto quit
 shutdown -s -t 0
 goto quit
 :s_R
-if "%2"=="F" goto sd_RF
-if "%2"=="f" goto sd_RF
+if /I "%2"=="F" goto sd_RF
 goto sd_RN
 goto quit
 :sd_RN
